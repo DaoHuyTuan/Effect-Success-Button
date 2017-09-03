e2cd75f43c86ca66a8aeb4abab3ceb348893a789
dao huy tuan <tuandh95@gmail.com>
2017-09-03 22:03:29 +0700

    [Done][Click Effect]

--- js/effect.js
+++ js/effect.js
@@ -1,6 +1,11 @@
 
 
 $(".button").click(function() {
-    $(".img-button").show();
+    $(".button").addClass("loader");
     $(".text-button").hide();
+    setInterval(function(){
+        $(".img-button").show();
+        
+        $(".button").removeClass("loader");
+    },5000);
 })
